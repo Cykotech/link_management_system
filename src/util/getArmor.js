a06863b87caabd617291257440d16a5a8106646b
Cykotech <cykotech.development@yahoo.com>
2024-03-02 01:07:21 -0500

    Restructure Armor Object

--- src/util/getArmor.js
+++ src/util/getArmor.js
@@ -5,37 +5,49 @@ export async function getArmor() {
     );
     const json = await response.json();
     const armors = [];
-    for (let i = 0; i < Object.keys(json).length; i++) {
-      armors.push({
-        name: json[i][0],
-        imgSrc: json[i][1],
-        set: json[i][2],
-        upgrades: {
-          level1: {
-            [json[i][3]]: json[i][4],
-            [json[i][5]]: json[i][6],
-            [json[i][7]]: json[i][8],
-          },
-          level2: {
-            [json[i][9]]: json[i][10],
-            [json[i][11]]: json[i][12],
-            [json[i][13]]: json[i][14],
-          },
-          level3: {
-            [json[i][15]]: json[i][16],
-            [json[i][17]]: json[i][18],
-            [json[i][19]]: json[i][20],
-          },
-          level4: {
-            [json[i][21]]: json[i][22],
-            [json[i][23]]: json[i][24],
-            [json[i][25]]: json[i][26],
-          },
+
+    for (let armorIndexInJson = 0; armorIndexInJson < Object.keys(json).length; armorIndexInJson++) {
+      const armor = {
+        name: json[armorIndexInJson][0],
+        imgSrc: json[armorIndexInJson][1],
+        set: json[armorIndexInJson][2],
+        upgrades: {},
+      };
+
+      for (let level = 0; level < 4; level++) {
+        armor.upgrades[level] = [];
+
+        for (let upgradeIndex = 0; armor.upgrades[level].length < 3; upgradeIndex += 2) {
+          let upgradeItemIndex;
+
+          switch (level) {
+            case 0:
+              upgradeItemIndex = 3 + upgradeIndex;
+              break;
+            case 1:
+              upgradeItemIndex = 9 + upgradeIndex;
+              break;
+            case 2:
+              upgradeItemIndex = 15 + upgradeIndex;
+              break;
+            case 3:
+              upgradeItemIndex = 21 + upgradeIndex;
+              break;
+          }
+
+          const upgradeQuantityIndex = upgradeItemIndex + 1;
+
+          armor.upgrades[level].push({
+            name: json[armorIndexInJson][upgradeItemIndex],
+            quantity: json[armorIndexInJson][upgradeQuantityIndex],
+          });
         }
-      })
+      }
+
+      armors.push(armor);
     }
 
-    armors.forEach(armor => console.log(typeof armor.upgrades.level3.Sapphire));
+    return armors;
   } catch (err) {
     console.log(err);
   }
