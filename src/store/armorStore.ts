7713720f684e0843e55922031ce0aaa20e2d7789
Cykotech <cykotech.development@yahoo.com>
2024-03-08 13:42:55 -0500

    Create Armor store

---
+++ src/store/armorStore.ts
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+
+import { getArmor } from "../util/getArmor";
+
+type ArmorState = {
+  name: String;
+  currentLevel: Number;
+  isObtained: boolean;
+};
+
+type State = {
+  armors: ArmorState[];
+};
+
+type Actions = {
+  setLevel: (armorToSet: ArmorState, level: number, state: State) => void;
+  populateArmor: () => Promise<void>;
+};
+
+const initialState: ArmorState[] = [];
+
+export const useArmorsStore = create<State & Actions>((set) => ({
+  armors: initialState,
+  setLevel: (armorToSet: ArmorState, level: number, state: State) => {
+    const armorIndex: number = state.armors.findIndex(
+      (armor) => armor.name === armorToSet.name
+    );
+
+    state.armors[armorIndex].currentLevel = level;
+
+    set((state) => ({
+      armors: [...state.armors],
+    }));
+  },
+  populateArmor: async () => {
+    const armors = await getArmor();
+    const armorsState: ArmorState[] = armors.map((armor) => ({
+      name: armor.name,
+      currentLevel: 0,
+      isObtained: armor.isObtained,
+    }));
+
+    set(() => ({
+      armors: armorsState,
+    }));
+  },
+}));
