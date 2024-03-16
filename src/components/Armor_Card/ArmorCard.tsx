f39252764e136aa52ddb52c8344fc8604ac12aad
Cykotech <cykotech.development@yahoo.com>
2024-03-16 15:42:27 -0400

    Create display toggles

--- src/components/Armor_Card/ArmorCard.tsx
+++ src/components/Armor_Card/ArmorCard.tsx
@@ -1,45 +1,76 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
+import { useEffect } from "react";
 
 import { Upgrades } from "./Upgrades/Upgrades";
 
-import { TUpgrades } from "../../util/getArmor";
+import { useArmorsStore } from "../../store/armorStore";
+
+import { TArmor } from "../../util/getArmor";
 import { TMaterial } from "../../util/getMaterials";
 
 import classes from "./ArmorCard.module.scss";
 
 type Props = {
-  children: string;
-  imgSrc: string;
+  armor: TArmor;
   materials: TMaterial[];
-  isUpgradeable: boolean;
-  upgrades?: TUpgrades | {};
+  children: string;
+  obtainDisplay: boolean;
+  upgradeDisplay: boolean;
 };
 
 export function ArmorCard({
-  children,
-  imgSrc,
-  upgrades,
   materials,
-  isUpgradeable,
+  armor,
+  children,
+  obtainDisplay,
+  upgradeDisplay,
 }: Props) {
+  const armorsStore = useArmorsStore((state) => state);
+  const { armors, obtainArmor } = useArmorsStore((state) => state);
+  const armorIndex = armorsStore.armors.findIndex(
+    (armor) => armor.name === children
+  );
+
+  useEffect(() => {
+    console.log(armor.isObtained);
+  }, [armor]);
+
   function canBeUpgraded() {
-    if (isUpgradeable) {
+    if (armor.isUpgradeable) {
       return (
         <Upgrades
           armorName={children}
-          upgrades={upgrades}
+          upgrades={armor.upgrades}
           materials={materials}
         />
       );
     }
   }
 
+  function upgradeClassToggle() {
+    if (!armor.isUpgradeable) {
+      return !upgradeDisplay ? classes.notUpgradeable : "";
+    }
+  }
+
+  function obtainClassToggle() {
+    if (!armor.isObtained) {
+      return obtainDisplay ? classes.notObtained : "";
+    }
+    // return armor.isObtained ? classes.obtained : classes.notObtained
+  }
+
   return (
-    <div className={classes.card}>
-      <img src={imgSrc}></img>
+    <div
+      className={`${classes.card} ${upgradeClassToggle()}
+       ${obtainClassToggle()}`}>
+      <img src={armor.imgSrc}></img>
       <h3>{children}</h3>
       {canBeUpgraded()}
+      <button onClick={() => obtainArmor(armor.name, armorsStore)}>
+        {!armors[armorIndex].isObtained ? "Obtain Armor" : "Unobtain Armor"}
+      </button>
     </div>
   );
 }
--- src/pages/Armor_Tracker/ArmorTracker.tsx
+++ src/pages/Armor_Tracker/ArmorTracker.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 
-// import { useInventoryStore } from "../../store/inventoryStore";
+import { useState } from "react";
 
 import { ArmorCard } from "../../components/Armor_Card/ArmorCard";
 
@@ -12,22 +12,41 @@ import classes from "./ArmorTracker.module.scss";
 type Props = {
   armors: TArmor[];
   materials: TMaterial[];
-}
+};
 
 export function ArmorTracker({ armors, materials }: Props) {
+  const [obtainDisplay, setObtainDisplay] = useState(true);
+  const [upgradeDisplay, setUpgradeDisplay] = useState(true);
+
+  function toggleObtainDisplay() {
+    setObtainDisplay(!obtainDisplay);
+  }
+
+  function toggleUpgradeDisplay() {
+    setUpgradeDisplay(!upgradeDisplay);
+  }
 
   return (
     <div className={classes.container}>
       <h2>Armor Tracker</h2>
+      <button
+        onClick={() => {
+          toggleUpgradeDisplay();
+        }}>
+        Toggle isUpgradeable Display
+      </button>
+      <button onClick={() => toggleObtainDisplay()}>
+        Toggle isObtained Display
+      </button>
       <div className={classes.grid}>
         {armors.map((armor) => {
           return (
             <ArmorCard
               key={armor.name}
-              imgSrc={armor.imgSrc}
-              upgrades={armor.upgrades}
               materials={materials}
-              isUpgradeable={armor.isUpgradeable}>
+              armor={armor}
+              obtainDisplay={obtainDisplay}
+              upgradeDisplay={upgradeDisplay}>
               {armor.name}
             </ArmorCard>
           );
