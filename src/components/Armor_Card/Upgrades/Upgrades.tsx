eef787461062db4fe6037075bbfd61e8419c3588
Cykotech <cykotech.development@yahoo.com>
2024-03-30 20:12:47 -0400

    Highlight current level on button

--- src/components/Armor_Card/Upgrades/Upgrades.tsx
+++ src/components/Armor_Card/Upgrades/Upgrades.tsx
@@ -1,11 +1,9 @@
 /* eslint-disable react/jsx-key */
 /* eslint-disable react/prop-types */
-import { useMemo } from "react";
+import { useMemo, useState, useEffect } from "react";
 
 import { useInventoryStore } from "../../../store/inventoryStore";
 import { useArmorsStore } from "../../../store/armorStore";
-
-import { upgradeArmor } from "../../../util/upgradeFunction";
 
 import { TUpgrades } from "../../../util/getArmor";
 
@@ -19,10 +17,11 @@ type TProps = {
 };
 
 export function Upgrades({ id, upgrades, armorName }: TProps) {
+  const [activeButton, setActiveButton] = useState(0);
+
   const armorsStore = useArmorsStore((state) => state);
   const { armors, setLevel } = useArmorsStore((state) => state);
 
-  const inventoryStore = useInventoryStore((state) => state);
   const { inventory } = useInventoryStore((state) => state);
 
   const armorToUpgrade = useMemo(
@@ -30,23 +29,9 @@ export function Upgrades({ id, upgrades, armorName }: TProps) {
     [armorName]
   );
 
-  function upgradeButton() {
-    if (armorToUpgrade.currentLevel !== 4) {
-      return (
-        <button
-          onClick={() => {
-            upgradeArmor(
-              armorToUpgrade,
-              armorToUpgrade.currentLevel,
-              inventoryStore,
-              armorsStore
-            );
-          }}>
-          Upgrade
-        </button>
-      );
-    }
-  }
+  useEffect(() => {
+    setActiveButton(armorToUpgrade.currentLevel);
+  }, [armorToUpgrade.currentLevel]);
 
   function displayUpgradeMaterials() {
     if (armorToUpgrade) {
@@ -79,30 +64,40 @@ export function Upgrades({ id, upgrades, armorName }: TProps) {
     <>
       <div className={classes.levelContainer}>
         <button
-          className={classes.button}
+          className={`${classes.button} ${
+            activeButton === 0 ? classes.active : ""
+          }`}
           onClick={() => setLevel(armorToUpgrade, 0, armorsStore)}>
           Base
         </button>
         <button
-          className={classes.button}
+          className={`${classes.button} ${
+            activeButton === 1 ? classes.active : ""
+          }`}
           onClick={() => setLevel(armorToUpgrade, 1, armorsStore)}>
           <img src={Star}></img>
         </button>
         <button
-          className={classes.button}
+          className={`${classes.button} ${
+            activeButton === 2 ? classes.active : ""
+          }`}
           onClick={() => setLevel(armorToUpgrade, 2, armorsStore)}>
           <img src={Star}></img>
           <img src={Star}></img>
         </button>
         <button
-          className={classes.button}
+          className={`${classes.button} ${
+            activeButton === 3 ? classes.active : ""
+          }`}
           onClick={() => setLevel(armorToUpgrade, 3, armorsStore)}>
           <img src={Star}></img>
           <img src={Star}></img>
           <img src={Star}></img>
         </button>
         <button
-          className={classes.button}
+          className={`${classes.button} ${
+            activeButton === 4 ? classes.active : ""
+          }`}
           onClick={() => setLevel(armorToUpgrade, 4, armorsStore)}>
           <img src={Star}></img>
           <img src={Star}></img>
@@ -112,7 +107,6 @@ export function Upgrades({ id, upgrades, armorName }: TProps) {
       </div>
 
       {displayUpgradeMaterials()}
-      {/* {upgradeButton()} */}
     </>
   );
 }
