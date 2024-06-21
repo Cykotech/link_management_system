4c91068e63ca5b783363f2f6d984ba945b6abe08
Cykotech <cykotech.development@yahoo.com>
2024-06-21 13:36:25 -0400

    Populate Ingredients

--- src/pages/Cookbook/Cookbook.tsx
+++ src/pages/Cookbook/Cookbook.tsx
@@ -1,5 +1,31 @@
-export function Cookbook() {
+import React from "react";
+import { useInventoryStore } from "../../store/inventoryStore";
+
+import classes from "./Cookbook.module.scss";
+
+interface Props {
+  style?: React.CSSProperties;
+}
+
+export function Cookbook({ style }: Props) {
+  const { inventory } = useInventoryStore();
+
   return (
-    <h2>Cookbook</h2>
-  )
+    <>
+      <div
+        style={style}
+        className={classes.ingredientContainer}>
+        {inventory.map((ingredient, i) => {
+          return (
+            <div
+              className={classes.ingredientCard}
+              key={`${ingredient.name}-${i}`}>
+              <img src={ingredient.imgSrc} />
+              <p>{ingredient.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
