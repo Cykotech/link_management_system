83ff9fe32961e55b357a7a27ac829547b1256742
Cykotech <cykotech.development@yahoo.com>
2024-06-21 15:18:51 -0400

    Add actions to RecipesStore

--- src/store/recipesStore.ts
+++ src/store/recipesStore.ts
@@ -4,20 +4,46 @@ import { getRecipes } from "../util/getRecipes";
 
 type State = {
   recipes: TRecipe[];
+  activeIngredients: TMaterial[];
+  activeRecipe: TRecipe | {};
 };
 
 type Actions = {
   populateRecipes: () => Promise<void>;
+  addIngredient: (ingredient: TMaterial, state: State) => void;
+  setActiveRecipe: () => void;
+  resetIngredients: () => void;
 };
 
-const initialState: TRecipe[] = [];
+const initialRecipeState: TRecipe[] = [];
+const initialIngredientState: TMaterial[] = [];
+const initialActiveRecipeState: TRecipe | {} = {};
 
 export const useRecipesStore = create<State & Actions>((set) => ({
-  recipes: initialState,
+  recipes: initialRecipeState,
+  activeIngredients: initialIngredientState,
+  activeRecipe: initialActiveRecipeState,
   populateRecipes: async () => {
     const recipes = await getRecipes();
     set(() => ({
       recipes: recipes,
+    }));
+  },
+  addIngredient: (ingredient, state) => {
+    if (state.activeIngredients.length === 5) {
+      return;
+    }
+
+    set((state) => ({
+      activeIngredients: [...state.activeIngredients, ingredient],
+    }));
+  },
+  setActiveRecipe: () => {
+
+  },
+  resetIngredients: () => {
+    set(() => ({
+      activeIngredients: [...initialIngredientState],
     }));
   },
 }));
