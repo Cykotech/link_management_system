12e1027d4a937396d78f4da1c37b83542844ecae
Cykotech <cykotech.development@yahoo.com>
2024-06-21 15:20:03 -0400

    Populate active ingredients

--- src/pages/Cookbook/Cookbook.tsx
+++ src/pages/Cookbook/Cookbook.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useInventoryStore } from "../../store/inventoryStore";
+import { useRecipesStore } from "../../store/recipesStore";
 
 import classes from "./Cookbook.module.scss";
 
@@ -9,23 +10,38 @@ interface Props {
 
 export function Cookbook({ style }: Props) {
   const { inventory } = useInventoryStore();
+  const recipesStore = useRecipesStore();
+  const { activeIngredients, addIngredient } = useRecipesStore(
+    (state) => state
+  );
 
   return (
-    <>
+    <div className={classes.cookbook} style={style}>
       <div
-        style={style}
         className={classes.ingredientContainer}>
         {inventory.map((ingredient, i) => {
           return (
             <div
               className={classes.ingredientCard}
-              key={`${ingredient.name}-${i}`}>
+              key={`${ingredient.name}-${i}`}
+              onClick={() => {
+                addIngredient(ingredient, recipesStore);
+              }}>
               <img src={ingredient.imgSrc} />
               <p>{ingredient.name}</p>
             </div>
           );
         })}
       </div>
-    </>
+      <div className={classes.recipeContainer}>
+        {activeIngredients.map((ingredient, i) => {
+          return (
+            <div className={classes.ingredientCard} key={i}>
+              <img src={ingredient.imgSrc} />
+            </div>
+          )
+        })}
+      </div>
+    </div>
   );
 }
