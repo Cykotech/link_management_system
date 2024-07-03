9778f30c640ef0565cb16a1976026eef245cc2c1
Cykotech <cykotech.development@yahoo.com>
2024-07-03 13:53:03 -0400

    Add buttons to Cookbook header

--- src/pages/Cookbook/CookbookHeader.tsx
+++ src/pages/Cookbook/CookbookHeader.tsx
@@ -1,7 +1,25 @@
 import classes from "./Cookbook.module.scss";
+import { useRecipesStore } from "../../store/recipesStore";
 
-export function CookbookHeader() {
+type Props = {
+  showModal: string;
+  modalClick: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export function CookbookHeader({ showModal, modalClick }: Props) {
+  const { resetIngredients } = useRecipesStore();
   return (
-    <h2 className={classes.header}>Cookbook</h2>
-    );
+    <div className={classes.header}>
+      <h2>Cookbook</h2>
+      <div className={classes.buttonContainer}>
+        <button className={classes.button}>How To Use</button>
+        <button className={classes.button}>Show All Recipes</button>
+        <button
+          className={classes.button}
+          onClick={() => resetIngredients()}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
 }
