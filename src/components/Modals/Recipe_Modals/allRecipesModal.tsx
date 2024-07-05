e7cf1068066ab6f58ff58c0a6e2c5ef639dbddf6
Cykotech <cykotech.development@yahoo.com>
2024-07-05 18:28:18 -0400

    Add cook button and Link Recipe Info modal

--- src/components/Modals/Recipe_Modals/allRecipesModal.tsx
+++ src/components/Modals/Recipe_Modals/allRecipesModal.tsx
@@ -3,22 +3,35 @@ import classes from "./recipeModals.module.scss";
 
 type Props = {
   showModal: string;
+  handleClick: React.Dispatch<React.SetStateAction<string>>;
   handleClose: React.Dispatch<React.SetStateAction<string>>;
+  handleInfoModal: React.Dispatch<React.SetStateAction<string>>;
 };
 
-export function AllRecipesModal({ showModal, handleClose }: Props) {
+export function AllRecipesModal({
+  showModal,
+  handleClick,
+  handleClose,
+  handleInfoModal,
+}: Props) {
   const { recipes } = useRecipesStore();
   return (
     showModal === "allRecipes" && (
-      <div className={classes.modal}>
+      <div className={`${classes.all} ${classes.modal}`}>
         <div className={classes.textContainer}>
           <h3>All Recipes</h3>
           <span onClick={() => handleClose("none")}>X</span>
         </div>
         <div className={classes.recipesContainer}>
-          {recipes.map((recipe) => {
+          {recipes.map((recipe, i) => {
             return (
-              <div className={classes.card}>
+              <div
+                className={classes.card}
+                key={`${recipe.name}-${i}`}
+                onClick={() => {
+                  handleInfoModal(recipe.name);
+                  handleClick("recipeInfo");
+                }}>
                 <img
                   src={recipe.imgSrc}
                   alt={recipe.name}
--- src/pages/Cookbook/Cookbook.tsx
+++ src/pages/Cookbook/Cookbook.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { ReactNode, useState } from "react";
 import { useInventoryStore } from "../../store/inventoryStore";
 import { useRecipesStore } from "../../store/recipesStore";
 import { RecipeModal } from "../../components/Modals/Recipe_Modals/recipeModal";
 import { AllRecipesModal } from "../../components/Modals/Recipe_Modals/allRecipesModal";
+import { RecipeInfoModal } from "../../components/Modals/Recipe_Modals/infoModal";
 
 import classes from "./Cookbook.module.scss";
 
@@ -13,16 +14,42 @@ interface Props {
 }
 
 export function Cookbook({ style, showModal, modalClick }: Props) {
-  const { inventory } = useInventoryStore();
+  const { inventory, consumeMaterials } = useInventoryStore();
+  const inventoryStore = useInventoryStore();
   const recipesStore = useRecipesStore();
-  const { activeIngredients, addIngredient } = useRecipesStore(
-    (state) => state
-  );
+  const { activeIngredients, addIngredient, recipes, resetIngredients } =
+    useRecipesStore((state) => state);
+
+  const [recipeInfo, setRecipeInfo] = useState("");
+
+  function cookRecipe() {
+    consumeMaterials(activeIngredients, inventoryStore);
+    resetIngredients();
+  }
+
+  function renderActiveRecipe(): ReactNode {
+    return (
+      <>
+        <div className={classes.recipeCard}>
+          <img
+            src={recipes[144].imgSrc}
+            alt={recipes[144].imgSrc}
+          />
+        </div>
+        <button
+          className={classes.button}
+          onClick={() => cookRecipe()}>
+          Cook
+        </button>
+      </>
+    );
+  }
 
   return (
-    <div className={classes.cookbook} style={style}>
-      <div
-        className={classes.ingredientContainer}>
+    <div
+      className={classes.cookbook}
+      style={style}>
+      <div className={classes.ingredientContainer}>
         {inventory.map((ingredient, i) => {
           return (
             <div
@@ -39,16 +66,32 @@ export function Cookbook({ style, showModal, modalClick }: Props) {
         })}
       </div>
       <div className={classes.recipeContainer}>
-        {activeIngredients.map((ingredient, i) => {
-          return (
-            <div className={classes.ingredientCard} key={i}>
-              <img src={ingredient.imgSrc} />
-            </div>
-          )
-        })}
+        <div className={classes.activeContainer}>
+          {activeIngredients.map((ingredient, i) => {
+            return (
+              <div
+                className={classes.ingredientCard}
+                key={i}>
+                <img src={ingredient.imgSrc} />
+              </div>
+            );
+          })}
+        </div>
+        {activeIngredients.length === 0 ? "" : renderActiveRecipe()}
       </div>
-      <RecipeModal showModal={showModal} handleClose={modalClick}></RecipeModal>
-      <AllRecipesModal showModal={showModal} handleClose={modalClick}></AllRecipesModal>
+      <RecipeModal
+        showModal={showModal}
+        handleClose={modalClick}></RecipeModal>
+      <AllRecipesModal
+        showModal={showModal}
+        handleInfoModal={setRecipeInfo}
+        handleClick={modalClick}
+        handleClose={modalClick}></AllRecipesModal>
+      <RecipeInfoModal
+        showModal={showModal}
+        showInfo={recipeInfo}
+        handleClick={modalClick}
+        handleClose={modalClick}></RecipeInfoModal>
     </div>
   );
 }
