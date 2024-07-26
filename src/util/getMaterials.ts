b6666607f09b5d1b4c9a85e7388491e847e9aaa7
Cykotech <cykotech.development@yahoo.com>
2024-07-26 15:54:36 -0400

    Assign Id's to materials

--- src/util/getMaterials.ts
+++ src/util/getMaterials.ts
@@ -4,14 +4,17 @@ export async function getMaterials(): Promise<TMaterial[]> {
       "https://script.google.com/macros/s/AKfycbxjYKCnx2VRAj7aphprnj6BMS36MQBEF9KSS8hBRJ8jqoAVhhQnZkEZLl5GRU304Z0/exec?type=materials"
     );
     const json = await response.json();
-    json.forEach(material => {
+    json.forEach((material) => {
       for (let property in material.ingredientProperties) {
         const numberValue = Number(material.ingredientProperties[property]);
         if (!Number.isNaN(numberValue)) {
           material.ingredientProperties[property] = numberValue;
         }
       }
-    })
+    });
+    for (let materialId = 0; materialId < json.length; materialId++) {
+      json[materialId].id = materialId;
+    }
     return json;
   } catch (err) {
     console.log(err);
