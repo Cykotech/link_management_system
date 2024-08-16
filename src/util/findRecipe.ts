0fd40bfaa849c29de8aafcc699c176478cdb8abe
Cykotech <cykotech.development@yahoo.com>
2024-08-16 13:35:07 -0400

    Find recipe with ingredients

--- src/util/findRecipe.ts
+++ src/util/findRecipe.ts
@@ -6,12 +6,49 @@ export function findRecipe(
   let recipeToFind = recipeName;
 
   if (ingredients) {
-    console.log(ingredients);
+    const meal: string[][] = [];
+    let filteredRecipes = recipes.filter(
+      (recipe) =>
+        recipe.ingredients.length <= ingredients.length &&
+        recipe.method === "Cooking Pot"
+    );
+
+    for (const ingredient of ingredients) {
+      meal.push([
+        ingredient.name,
+        ingredient.cookingTags[0] === "CookLowPrice"
+          ? ingredient.cookingTags[2]
+          : ingredient.cookingTags[1],
+        ingredient.cookingTags.custom1,
+        ingredient.cookingTags.custom2,
+      ]);
+    }
+
+    if (meal) {
+      console.log(meal);
+      console.log("original filtered recipes", filteredRecipes);
+      for (
+        let ingredientIndex = 0;
+        ingredientIndex < meal.length;
+        ingredientIndex++
+      ) {
+        filteredRecipes = filteredRecipes.filter((recipe) => {
+          return meal[ingredientIndex].some((value) => {
+            return recipe.ingredients.includes(value);
+          });
+        });
+      }
+      console.log("new filtered recipes", filteredRecipes);
+    }
+
+    return filteredRecipes[0];
   }
 
-  const recipeIndex = recipes.findIndex(
-    (recipe) => recipe.name === recipeToFind
-  );
+  if (!ingredients) {
+    const recipeIndex = recipes.findIndex(
+      (recipe) => recipe.name === recipeToFind
+    );
 
-  return recipes[recipeIndex];
+    return recipes[recipeIndex];
+  }
 }
