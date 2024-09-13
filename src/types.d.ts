82d5f576c177ce6beff31eaa18db12a251d5680e
Cykotech <cykotech.development@yahoo.com>
2024-09-13 14:32:41 -0400

    Add TDevice and reformat other types

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,6 +1,7 @@
 interface TRecipe {
   name: string;
   imgSrc: string;
+  actorName: string;
   ingredients: string[];
   method: string;
   effect: string;
@@ -11,44 +12,53 @@ interface TRecipe {
 }
 
 interface TMaterial {
-  name: string,
-  imgSrc: string,
-  ingredientProperties: Properties,
-  cookingTags: Tags,
-  quantity: number,
-  id: number
+  name: string;
+  imgSrc: string;
+  actorName: string;
+  ingredientProperties: Properties;
+  cookingTags: Tags;
+  quantity: number;
+  id: number;
 }
 
 interface Properties {
-  heartsRecovered: number,
-  cookingEffect: string,
-  effectLevel: number,
-  effectDuration: number,
-  durationBoost: number,
-  critChance: number
+  heartsRecovered: number;
+  cookingEffect: string;
+  effectLevel: number;
+  effectDuration: number;
+  durationBoost: number;
+  critChance: number;
 }
 
 interface Tags {
-  1: string,
-  2: string,
-  3: string,
-  custom1: string,
-  custom2: string
+  1: string;
+  2: string;
+  3: string;
+  custom1: string;
+  custom2: string;
 }
 
 interface TArmor {
-  name: string,
-  imgSrc: string,
-  set: string,
-  upgrades: TUpgrades | {},
-  isObtained: boolean,
-  isUpgradeable: boolean
+  name: string;
+  imgSrc: string;
+  set: string;
+  upgrades: TUpgrades | {};
+  isObtained: boolean;
+  isUpgradeable: boolean;
 }
 
 interface TUpgrades {
-  0: TMaterial[],
-  1: TMaterial[],
-  2: TMaterial[],
-  3: TMaterial[],
-  4: [],
+  0: TMaterial[];
+  1: TMaterial[];
+  2: TMaterial[];
+  3: TMaterial[];
+  4: [];
+}
+
+interface TDevice {
+  name: string;
+  imgSrc: string[];
+  duration: number;
+  energyConsumption: number;
+  consumptionPerSecond: number;
 }
